9105398d3fe8dec4913fc4be9adb24327a9f87c5
TediusHub <88737099+TediusHub@users.noreply.github.com>
2021-08-10 14:15:36 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,36 @@
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/TediusHub/TediusHublua/main/Module.lua"))()
+
+local UI = Material.Load({
+     Title = "Tedius Hub",
+     Style = 24,
+     SizeX = 500,
+     SizeY = 200,
+     Theme = "Dark"
+})
+
+local Page1 = UI.New({
+    Title = "Home",
+})
+
+local Page2 = UI.New({
+    Title = "Credits",
+})
+
+Page1.Button({
+    Text = "Pet Simulator X",
+    Callback = function()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/Wind596/RaitoHub.c-/main/RaitoHub", true))()
+end
+})
+
+Page2.Button({
+    Text = "Owner: Zaiddd#1111",
+    Callback = function()
+end
+})
+
+Page2.Button({
+    Text = "UI Designer: Zaiddd#1111",
+    Callback = function()
+end
+})
